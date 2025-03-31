1b11bebda4ae04c39a5790c316c240f8fe426f5a
shira rap <8041shira@gmail.com>
2025-04-01 00:37:10 +0300

    feat:add gitignore file

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const Route_User = require('./Routes/Route_User');
-const Route_Product = require('./Routes/Route_Product');
+//const Route_User = require('./Routes/Route_User');
+//const Route_Product = require('./Routes/Route_Product');
 
 dotenv.config();
 
@@ -11,8 +11,8 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/api/user', Route_User);
-app.use('/api/product', Route_Product);
+//app.use('/api/user', Route_User);
+//app.use('/api/product', Route_Product);
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
