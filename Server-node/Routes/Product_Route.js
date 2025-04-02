688cfea5bff4ef0f538452c796ee17c0ad3bc9d7
shira rap <8041shira@gmail.com>
2025-04-02 16:11:14 +0300

    feat:Add Product_Route

---
+++ Server-node/Routes/Product_Route.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { createProduct, getProduct, updateProduct, deleteProduct,getAllProduct } = require('../Controllers/Controller_Product');
+const { User } = require('../Middleware/UserMiddleware');
+const router = express.Router();
+
+router.post('/', User, createProduct);
+router.get('/', User, getAllProduct);
+router.get('/:id', User, getProduct);
+router.put('/:id', User, updateProduct);
+router.delete('/:id', User, deleteProduct);
+
+module.exports = router;
